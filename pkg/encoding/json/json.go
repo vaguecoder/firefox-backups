7f7bf5a9a6b7e654631f0291f0e4c4cf79bf6a3d
Bhargav Ravuri <bhargav.ravuri@infracloud.io>
2023-01-29 00:02:14 +0530

    Add Encoding/JSON Package's Tests and Comments
    Signed-off-by: Bhargav Ravuri <vaguecoder0to.n@gmail.com>

--- pkg/encoding/json/json.go
+++ pkg/encoding/json/json.go
@@ -11,31 +11,45 @@ import (
 	"github.com/vaguecoder/firefox-backups/pkg/files"
 )
 
+// indentation is the JSON indentation string
+const indentation = "\t"
+
+// EncoderName is name of the encoder in current package, i.e., JSON.
+// JSONFormat constant is parsed as EncoderName type here.
 var EncoderName = encoding.ToEncoder(constants.JSONFormat)
 
 func init() {
+	// Register the encoder name in pkg/encoding.AllEncoders
 	encoding.AllEncoders = append(encoding.AllEncoders, EncoderName)
 }
 
+// Encoder is the manager for JSON encoder
 type Encoder struct {
 	jsonEncoder *json.Encoder
 	filename    string
 }
 
+// NewEncoder initializes new Encoder
 func NewEncoder(out io.Writer) *Encoder {
 	var filename string
+
+	// If the output stream is a file, specifically pkg/files.File,
+	// the filename can be extracted here. Just an optional requirement.
 	if file, ok := any(out).(files.File); ok {
 		filename = file.Name()
 	}
 
+	// Create new JSON encoder and set the indentation per constant.
 	encoder := json.NewEncoder(out)
-	encoder.SetIndent("", "\t")
+	encoder.SetIndent("", indentation)
+
 	return &Encoder{
 		jsonEncoder: encoder,
 		filename:    filename,
 	}
 }
 
+// Encode encodes the input bookmarks in JSON format to already set output stream
 func (e *Encoder) Encode(bookmarks []bookmark.Bookmark) error {
 	err := e.jsonEncoder.Encode(bookmarks)
 	if err != nil {
@@ -45,10 +59,14 @@ func (e *Encoder) Encode(bookmarks []bookmark.Bookmark) error {
 	return nil
 }
 
+// String returns the encoder name derived in EncoderName.
+// This returns the same value as EncoderName, but using the receiver.
 func (e *Encoder) String() string {
 	return EncoderName.String()
 }
 
+// Filename returns the file name string derived from output stream,
+// iff the output stream is of pkg/files.File type.
 func (e *Encoder) Filename() string {
 	return e.filename
 }
--- pkg/encoding/json/json_test.go
+++ pkg/encoding/json/json_test.go
@@ -1,1 +1,318 @@
 package json
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/vaguecoder/firefox-backups/pkg/bookmark"
+	"github.com/vaguecoder/firefox-backups/pkg/constants"
+	"github.com/vaguecoder/firefox-backups/pkg/mocks"
+	"github.com/vaguecoder/firefox-backups/pkg/util"
+)
+
+var (
+	ptrStr                 = util.PtrStr
+	stringSliceToFlatBytes = util.StringSliceToFlatBytes
+)
+
+func TestEncoder_Encode(t *testing.T) {
+	type fields struct {
+		filename     string
+		outputWriter io.Writer
+	}
+
+	type args struct {
+		bookmarks []bookmark.Bookmark
+	}
+
+	type toggles struct {
+		isExpectedError    bool
+		isFileWriter       bool
+		isErrorAtJSONWrite bool
+	}
+
+	type testData struct {
+		name     string
+		fields   fields
+		args     args
+		toggles  toggles
+		expected []string
+	}
+
+	var (
+		err            error
+		tests          []testData
+		testCase       testData
+		encoder        *Encoder
+		filename       string
+		expectedOutput []byte
+
+		fileWriter    = new(mocks.File)
+		nonFileWriter = new(mocks.NonFileWriter)
+	)
+
+	tests = []testData{
+		{
+			name: "Valid-Case",
+			fields: fields{
+				filename:     "firefox-bookmarks.json",
+				outputWriter: fileWriter,
+			},
+			args: args{
+				bookmarks: []bookmark.Bookmark{
+					{
+						URL:    ptrStr("https://github.com/vaguecoder"),
+						Title:  "Vague Coder",
+						Folder: "Profiles/GitHub",
+						ID:     1,
+						Parent: 0,
+					},
+					{
+						URL:    ptrStr("https://github.com/random"),
+						Title:  "Random",
+						Folder: "Profiles/GitHub",
+						ID:     2,
+						Parent: 0,
+					},
+				},
+			},
+			toggles: toggles{
+				isExpectedError:    false,
+				isFileWriter:       true,
+				isErrorAtJSONWrite: false,
+			},
+			expected: []string{
+				`[`,
+				`	{`,
+				`		"url": "https://github.com/vaguecoder",`,
+				`		"title": "Vague Coder",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 1,`,
+				`		"parent": 0`,
+				`	},`,
+				`	{`,
+				`		"url": "https://github.com/random",`,
+				`		"title": "Random",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 2,`,
+				`		"parent": 0`,
+				`	}`,
+				`]`,
+			},
+		},
+		{
+			name: "Valid-Case-With-Non-File-Writer",
+			fields: fields{
+				filename:     "firefox-bookmarks.json",
+				outputWriter: nonFileWriter,
+			},
+			args: args{
+				bookmarks: []bookmark.Bookmark{
+					{
+						URL:    ptrStr("https://github.com/vaguecoder"),
+						Title:  "Vague Coder",
+						Folder: "Profiles/GitHub",
+						ID:     1,
+						Parent: 0,
+					},
+					{
+						URL:    ptrStr("https://github.com/random"),
+						Title:  "Random",
+						Folder: "Profiles/GitHub",
+						ID:     2,
+						Parent: 0,
+					},
+				},
+			},
+			expected: []string{
+				`[`,
+				`	{`,
+				`		"url": "https://github.com/vaguecoder",`,
+				`		"title": "Vague Coder",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 1,`,
+				`		"parent": 0`,
+				`	},`,
+				`	{`,
+				`		"url": "https://github.com/random",`,
+				`		"title": "Random",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 2,`,
+				`		"parent": 0`,
+				`	}`,
+				`]`,
+			},
+			toggles: toggles{
+				isExpectedError:    false,
+				isFileWriter:       false,
+				isErrorAtJSONWrite: false,
+			},
+		},
+		{
+			name: "No-Bookmarks",
+			fields: fields{
+				filename:     "firefox-bookmarks.json",
+				outputWriter: fileWriter,
+			},
+			args: args{
+				bookmarks: []bookmark.Bookmark{},
+			},
+			toggles: toggles{
+				isExpectedError:    false,
+				isFileWriter:       true,
+				isErrorAtJSONWrite: false,
+			},
+			expected: []string{`[]`},
+		},
+		{
+			name: "No-Bookmarks-With-Non-File-Writer",
+			fields: fields{
+				filename:     "firefox-bookmarks.json",
+				outputWriter: nonFileWriter,
+			},
+			args: args{
+				bookmarks: []bookmark.Bookmark{},
+			},
+			expected: []string{`[]`},
+			toggles: toggles{
+				isExpectedError:    false,
+				isFileWriter:       false,
+				isErrorAtJSONWrite: false,
+			},
+		},
+		{
+			name: "Failure-At-JSON-Write-All",
+			fields: fields{
+				filename:     "firefox-bookmarks.json",
+				outputWriter: fileWriter,
+			},
+			args: args{
+				bookmarks: []bookmark.Bookmark{
+					{
+						URL:    ptrStr("https://github.com/vaguecoder"),
+						Title:  "Vague Coder",
+						Folder: "Profiles/GitHub",
+						ID:     1,
+						Parent: 0,
+					},
+					{
+						URL:    ptrStr("https://github.com/random"),
+						Title:  "Random",
+						Folder: "Profiles/GitHub",
+						ID:     2,
+						Parent: 0,
+					},
+				},
+			},
+			expected: []string{
+				`[`,
+				`	{`,
+				`		"url": "https://github.com/vaguecoder",`,
+				`		"title": "Vague Coder",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 1,`,
+				`		"parent": 0`,
+				`	},`,
+				`	{`,
+				`		"url": "https://github.com/random",`,
+				`		"title": "Random",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 2,`,
+				`		"parent": 0`,
+				`	}`,
+				`]`,
+			},
+			toggles: toggles{
+				isExpectedError:    true,
+				isFileWriter:       true,
+				isErrorAtJSONWrite: true,
+			},
+		},
+		{
+			name: "Failure-At-JSON-Write-All-With-Non-File-Writer",
+			fields: fields{
+				filename:     "firefox-bookmarks.json",
+				outputWriter: nonFileWriter,
+			},
+			args: args{
+				bookmarks: []bookmark.Bookmark{
+					{
+						URL:    ptrStr("https://github.com/vaguecoder"),
+						Title:  "Vague Coder",
+						Folder: "Profiles/GitHub",
+						ID:     1,
+						Parent: 0,
+					},
+					{
+						URL:    ptrStr("https://github.com/random"),
+						Title:  "Random",
+						Folder: "Profiles/GitHub",
+						ID:     2,
+						Parent: 0,
+					},
+				},
+			},
+			expected: []string{
+				`[`,
+				`	{`,
+				`		"url": "https://github.com/vaguecoder",`,
+				`		"title": "Vague Coder",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 1,`,
+				`		"parent": 0`,
+				`	},`,
+				`	{`,
+				`		"url": "https://github.com/random",`,
+				`		"title": "Random",`,
+				`		"folder": "Profiles/GitHub",`,
+				`		"id": 2,`,
+				`		"parent": 0`,
+				`	}`,
+				`]`,
+			},
+			toggles: toggles{
+				isExpectedError:    true,
+				isFileWriter:       false,
+				isErrorAtJSONWrite: true,
+			},
+		},
+	}
+	for _, testCase = range tests {
+		t.Run(testCase.name, func(t *testing.T) {
+			expectedOutput = stringSliceToFlatBytes(testCase.expected)
+
+			var (
+				lengthOfExpectedOutput       = len(expectedOutput)
+				jsonWriteAllErr        error = nil
+			)
+
+			if testCase.toggles.isErrorAtJSONWrite {
+				jsonWriteAllErr = fmt.Errorf("some error")
+			}
+
+			filename = ""
+			if testCase.toggles.isFileWriter {
+				filename = testCase.fields.filename
+				fileWriter.On("Name").Return(testCase.fields.filename).Once()
+				fileWriter.On("Write", expectedOutput).Return(lengthOfExpectedOutput, jsonWriteAllErr).Once()
+			} else {
+				nonFileWriter.On("Write", expectedOutput).Return(lengthOfExpectedOutput, jsonWriteAllErr).Once()
+			}
+
+			encoder = NewEncoder(testCase.fields.outputWriter)
+
+			err = encoder.Encode(testCase.args.bookmarks)
+
+			if testCase.toggles.isExpectedError {
+				assert.Error(t, err, "Expected error from encode")
+			} else {
+				assert.NoError(t, err, "Unexpected error from encode")
+			}
+
+			assert.Equal(t, filename, encoder.Filename(), "Mismatch of filename in writer")
+			assert.Equal(t, constants.JSONFormat.String(), encoder.String(), "Mismatch of encoder name string")
+		})
+	}
+}
